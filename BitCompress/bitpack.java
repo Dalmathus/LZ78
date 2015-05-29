b1bbf536f1f57ecd29cfae443c94264b46490c85
Dalmathus <jameswluxton@gmail.com>
2015-05-29 19:13:12 +1200

    Bitpacking Module
    Bit Packer and Bit Unpacker that handles LZ78 Tuples

---
+++ BitCompress/bitpack.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class bitpack {
+
+	public static void main(String[] args) throws IOException{ 
+		bit bp = new bit();
+		bp.pack();
+		System.out.println("Bitpacking Complete.");
+	}	
+}
---
+++ BitDecompress/bitunpack.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class bitunpack {
+
+	public static void main(String[] args) throws IOException {		
+		bit bd = new bit();
+		bd.unpack();
+		System.out.println("Unpacking Complete.");
+	}	
+}
